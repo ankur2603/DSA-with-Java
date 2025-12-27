d23742e0a83aa0c68767e73cea268c70202cb219
Ankur <ankur.career.2006@gmail.com>
2025-12-28 00:03:42 +0530

    Create longcommonprefix_14_leetcode.java

---
+++ STRINGS/longcommonprefix_14_leetcode.java
@@ -0,0 +1,27 @@
+Link : https://leetcode.com/problems/longest-common-prefix/submissions/1860014777/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String str1 = strs[0];
+        String str2 = strs[0];
+        for(int i = 0;i < strs.length;i++){
+            if(strs[i].compareTo(str1) < 0){
+                str1 = strs[i];
+            }
+            if(strs[i].compareTo(str2) > 0){
+                str2 = strs[i];
+            }
+        }
+        StringBuilder prefix = new StringBuilder();
+        int min = Math.min(str1.length(),str2.length());
+        for(int i = 0;i < min;i++){
+            if(str1.charAt(i) == str2.charAt(i)){
+                prefix.append(str1.charAt(i));
+            }
+            else{
+                return prefix.toString();
+            }
+        }
+        return prefix.toString();
+    }
+}
