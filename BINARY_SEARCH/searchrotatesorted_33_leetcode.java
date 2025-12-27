46294d66222b1ee7930098ce8fd11c4026bf4eb8
Ankur <ankur.career.2006@gmail.com>
2025-12-27 23:53:12 +0530

    Update searchrotatesorted_33_leetcode.java

--- BINARY_SEARCH/searchrotatesorted_33_leetcode.java
+++ BINARY_SEARCH/searchrotatesorted_33_leetcode.java
@@ -1,1 +1,32 @@
+Link : https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 
+class Solution {
+    public int search(int[] nums, int target) {
+        int si = 0;
+        int ei = nums.length-1;
+        int mid = 0;
+        while(si <= ei){
+            mid = si + (ei-si) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[si] <= nums[mid]){// Line 1
+                if(target <= nums[mid] && target >= nums[si]){
+                    ei = mid-1;
+                }
+                else{
+                    si = mid+1;
+                }
+            }
+            else{                      // Line 2
+                if(target <= nums[ei] && target >= nums[mid]){
+                    si = mid+1;
+                }
+                else{
+                    ei = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
