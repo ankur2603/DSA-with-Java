4d4ba904df4a707d01db0f9b0baa4c5707def285
Ankur <ankur.career.2006@gmail.com>
2025-12-27 23:18:15 +0530

    Add solution for max profit stock trading problem
    Implement solution for the best time to buy and sell stock problem.

---
+++ ARRAYS/stockbuy&sell_121_leetcode.java
@@ -0,0 +1,18 @@
+Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minprice=Integer.MAX_VALUE;
+        int maxprofit=0;
+        for(int i=0;i<prices.length;i++){
+            int curr=prices[i];
+            if(curr<minprice){
+                minprice=curr;
+            }
+            if(curr-minprice>maxprofit){
+                maxprofit=curr-minprice;
+            }
+        }
+        return maxprofit;
+    }
+}
