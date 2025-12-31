f8e05a430ebdf4f648855429cf8341a210456d9d
Ankur <ankur.career.2006@gmail.com>
2026-01-01 01:50:14 +0530

    Update subarrsumequalsk_560_leetcode.java

--- ARRAYS/subarrsumequalsk_560_leetcode.java
+++ ARRAYS/subarrsumequalsk_560_leetcode.java
@@ -2,10 +2,14 @@ Link : https://leetcode.com/problems/subarray-sum-equals-k/description/
 
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        
         HashMap<Integer,Integer>map = new HashMap<>();
+        
         int count = 0;
         int currSum = 0;
+        
         map.put(0,1);
+        
         for(int j = 0;j < nums.length;j++){
             currSum += nums[j];
 
