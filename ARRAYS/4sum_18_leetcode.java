b027c5a7cedf1286dfdece99324a84cad96a69bd
Ankur <ankur.career.2006@gmail.com>
2025-12-31 21:34:59 +0530

    Update 4sum_18_leetcode.java

--- ARRAYS/4sum_18_leetcode.java
+++ ARRAYS/4sum_18_leetcode.java
@@ -11,25 +11,25 @@ class Solution {
 
         for (int i = 0; i < n; i++){
             
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip for duplicate i
             
             for(int j = i+1;j < n;j++){
                 
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // skip for duplicate j 
                 
                 int k = j+1;
                 int l = n-1;
                 
                 while(k < l){
                     
-                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l]; // use long to avoid overflow
 
-                    if(sum == target){
+                    if(sum == target){ // favaourable case
                         
                         ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         
-                        while (k < l && nums[k] == nums[k + 1]) k++;
-                        while (k < l && nums[l] == nums[l - 1]) l--;
+                        while (k < l && nums[k] == nums[k + 1]) k++; // skip for duplicate k
+                        while (k < l && nums[l] == nums[l - 1]) l--; // skip for duplicate l
                         
                         k++;
                         l--;
@@ -37,7 +37,7 @@ class Solution {
 
                     else if(sum < target) k++;
 
-                    else l--;
+                    else l--; // sum is greater than the target
                 }
             }
         }
