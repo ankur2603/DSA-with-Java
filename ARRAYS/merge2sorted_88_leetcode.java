38f32fcf5f0b32b873556fc720fc63815ff075a1
Ankur <ankur.career.2006@gmail.com>
2025-12-31 12:41:15 +0530

    Update merge2sorted_88_leetcode.java

--- ARRAYS/merge2sorted_88_leetcode.java
+++ ARRAYS/merge2sorted_88_leetcode.java
@@ -6,22 +6,20 @@ class Solution {
         int i = m - 1;
         int j = n - 1;
         while(i >= 0 && j >= 0){
+
             if(nums1[i] > nums2[j]){
-                nums1[idx] = nums1[i];
-                i--;
+                nums1[idx--] = nums1[i--];
             }
+
             else{
-                nums1[idx] = nums2[j];
-                j--;
+                nums1[idx--] = nums2[j--];
             }
-            idx--;
+
         }
-        if(j >= 0){
-            while(j >= 0){
-                nums1[idx] = nums2[j];
-                j--;
-                idx--;
-            }
+
+        while(j >= 0){
+            nums1[idx--] = nums2[j--];
         }
+
     }
 }
