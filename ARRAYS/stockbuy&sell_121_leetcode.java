2762a77f287259716c0cbbed2087c9aeb19dada2
Ankur <ankur.career.2006@gmail.com>
2025-12-31 13:16:10 +0530

    Update stockbuy&sell_121_leetcode.java

--- ARRAYS/stockbuy&sell_121_leetcode.java
+++ ARRAYS/stockbuy&sell_121_leetcode.java
@@ -3,7 +3,7 @@ Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int minprice=Integer.MAX_VALUE;
+        int minprice=Integer.MAX_VALUE; // keep the track of minprice and maxprofit
         int maxprofit=0;
         
         for(int i=0;i<prices.length;i++){
