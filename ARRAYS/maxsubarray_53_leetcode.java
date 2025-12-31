2a32337c7f63b4dee13f107f043a23cd98c0736e
Ankur <ankur.career.2006@gmail.com>
2025-12-31 13:13:48 +0530

    Update maxsubarray_53_leetcode.java

--- ARRAYS/maxsubarray_53_leetcode.java
+++ ARRAYS/maxsubarray_53_leetcode.java
@@ -2,13 +2,18 @@ Link : https://leetcode.com/problems/maximum-subarray/description/
 
 class Solution {
     public int maxSubArray(int[] nums) {
+        
         int currs = 0;
         int maxs = Integer.MIN_VALUE;
+        
         for(int i = 0;i < nums.length;i++){
+            
             currs += nums[i];
+            
             if(currs > maxs){
                 maxs = currs;
             }
+            
             if(currs < 0){
                 currs = 0;
             }
