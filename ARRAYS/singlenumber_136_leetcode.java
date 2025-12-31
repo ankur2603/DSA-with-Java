645d5ea224be66ef18efafe840bbf7a17d3a6964
Ankur <ankur.career.2006@gmail.com>
2025-12-31 13:06:19 +0530

    Update singlenumber_136_leetcode.java

--- ARRAYS/singlenumber_136_leetcode.java
+++ ARRAYS/singlenumber_136_leetcode.java
@@ -2,10 +2,13 @@ Link : https://leetcode.com/problems/single-number/description/
 
 class Solution {
     public int singleNumber(int[] nums) {
+        
         int ans=0; // 0 ^ x = x & x ^ x = 0 basically xor on same elements is 0 on different elements is 1
+        
         for(int i=0;i<nums.length;i++){
             ans=ans^nums[i];
         }
+        
         return ans;
     }
 }
