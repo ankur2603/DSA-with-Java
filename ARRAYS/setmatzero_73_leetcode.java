be70746cf826a46feeb1de9ea2659db992083d4d
Ankur <ankur.career.2006@gmail.com>
2026-01-01 01:11:34 +0530

    Update setmatzero_73_leetcode.java

--- ARRAYS/setmatzero_73_leetcode.java
+++ ARRAYS/setmatzero_73_leetcode.java
@@ -2,30 +2,32 @@ Link : https://leetcode.com/problems/set-matrix-zeroes/description/
 
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean rflag[] = new boolean[m];
-        boolean cflag[] = new boolean[n];
+        
+        int m = matrix.length; // number of rows
+        int n = matrix[0].length; // number of cols
+        
+        boolean rflag[] = new boolean[m]; // used to store which row has zero
+        boolean cflag[] = new boolean[n]; // used to store which col has zero
 
-        for(int i = 0;i < m;i++){
+        for(int i = 0;i < m;i++){ // search for zeroes
             for(int j = 0;j < n;j++){
-                if(matrix[i][j] == 0){
+                if(matrix[i][j] == 0){ // if zero found -> true
                     rflag[i] = true;
                     cflag[j] = true;
                 }
             }
         }
 
-        for(int i = 0;i < m;i++){
+        for(int i = 0;i < m;i++){ 
             if(rflag[i] == true){
-                for(int j = 0;j < n;j++){
+                for(int j = 0;j < n;j++){ // fill entire col with zero
                     matrix[i][j] = 0;
                 }
             }
         }
         for(int j = 0;j < n;j++){
             if(cflag[j] == true){
-                for(int i = 0;i < m;i++){
+                for(int i = 0;i < m;i++){ // fill entire row with zero
                     matrix[i][j] = 0;
                 }
             }
