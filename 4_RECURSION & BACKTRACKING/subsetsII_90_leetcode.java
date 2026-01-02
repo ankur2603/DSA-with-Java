01fff18762204af718b5e1b0b4eab79d5bd04412
Ankur <ankur.career.2006@gmail.com>
2026-01-02 21:11:56 +0530

    Create subsetsII_90_leetcode.java

---
+++ 4_RECURSION & BACKTRACKING/subsetsII_90_leetcode.java
@@ -0,0 +1,49 @@
+Link : https://leetcode.com/problems/subsets-ii/description/
+
+import java.util.*;
+
+class Solution {
+
+    // Stores all unique subsets
+    public static List<List<Integer>> result = new ArrayList<>();
+
+    public static void findSub(int[] nums, List<Integer> ans, int i) {
+
+        // Every recursive call represents a valid subset
+        // So we add a COPY of the current subset
+        result.add(new ArrayList<>(ans));
+
+        // Try to include elements starting from index i
+        for (int idx = i; idx < nums.length; idx++) {
+
+            // Skip duplicates at the SAME recursion level
+            // This avoids generating identical subsets
+            if (idx > i && nums[idx] == nums[idx - 1]) {
+                continue;
+            }
+
+            // Choose current element
+            ans.add(nums[idx]);
+
+            // Recurse to build further
+            findSub(nums, ans, idx + 1);
+
+            // Backtrack: remove last added element
+            ans.remove(ans.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        // Sorting is required so duplicates come together
+        Arrays.sort(nums);
+
+        // Clear global result for safety
+        result.clear();
+
+        // Start recursion with empty subset
+        findSub(nums, new ArrayList<>(), 0);
+
+        return result;
+    }
+}
