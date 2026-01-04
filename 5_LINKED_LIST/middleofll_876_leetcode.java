2c929f95ef816097bb052656d80fe70666f2d888
Ankur <ankur.career.2006@gmail.com>
2026-01-05 01:37:46 +0530

    Create middleofll_876_leetcode.java

---
+++ 5_LINKED_LIST/middleofll_876_leetcode.java
@@ -0,0 +1,17 @@
+Link : https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head ;
+        ListNode fast = head ;
+        
+        while(fast != null && fast.next != null){
+
+            slow = slow.next; // +1
+            fast = (fast.next).next; // +2
+        }
+        
+        return slow;
+    }
+}
